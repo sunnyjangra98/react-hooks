41b4e1f5ab5b1eff0b64ed43372778b9c8eec561
DUSHYANT  JANGRA <dushyant16035@iiitd.ac.in>
2023-02-17 09:29:16 +0000

    aleternate way to handle edit instead of useEffect

--- src/forms/EditUserForm.js
+++ src/forms/EditUserForm.js
@@ -1,40 +1,52 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-const EditUserForm = props => {
-  const [ user, setUser ] = useState(props.currentUser)
-
-  useEffect(
+const EditUserForm = (props) => {
+  console.log("Rendering edit page for - ", props.currentUser);
+  /*useEffect(
     () => {
       setUser(props.currentUser)
     },
     [ props ]
-  )
+  )*/
   // You can tell React to skip applying an effect if certain values haven’t changed between re-renders. [ props ]
 
-  const handleInputChange = event => {
-    const { name, value } = event.target
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
 
-    setUser({ ...user, [name]: value })
-  }
+    props.setUser({ ...props.user, [name]: value });
+  };
 
   return (
     <form
-      onSubmit={event => {
-        event.preventDefault()
+      onSubmit={(event) => {
+        event.preventDefault();
 
-        props.updateUser(user.id, user)
+        props.updateUser(props.user.id, props.user);
       }}
     >
       <label>Name</label>
-      <input type="text" name="name" value={user.name} onChange={handleInputChange} />
+      <input
+        type="text"
+        name="name"
+        value={props.user.name}
+        onChange={handleInputChange}
+      />
       <label>Username</label>
-      <input type="text" name="username" value={user.username} onChange={handleInputChange} />
+      <input
+        type="text"
+        name="username"
+        value={props.user.username}
+        onChange={handleInputChange}
+      />
       <button>Update user</button>
-      <button onClick={() => props.setEditing(false)} className="button muted-button">
+      <button
+        onClick={() => props.setEditing(false)}
+        className="button muted-button"
+      >
         Cancel
       </button>
     </form>
-  )
-}
+  );
+};
 
-export default EditUserForm
+export default EditUserForm;
